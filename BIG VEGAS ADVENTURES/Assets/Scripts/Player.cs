ad7960d2f4887e9f093abb47cb4879497cb45f66
wthompson2 <wthompson@unomaha.edu>
2020-11-08 13:24:28 -0600

    adding Big Vegas scripts

---
+++ BIG VEGAS ADVENTURES/Assets/Scripts/Player.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float baseSpeed;
+    public float runningAmplifier;
+
+    public bool lockCursor;
+
+    private CharacterController characterController;
+    private Animator animator;
+
+    private float airTime;
+    private float gravity = -9.81f;
+
+    private PlayerMovementInfo playerMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+
+        playerMovement = new PlayerMovementInfo();
+        playerMovement.baseSpeed = baseSpeed;
+        playerMovement.runningAmplifier = runningAmplifier;
+
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        airTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+
+        PerformBlendTreeAnimation();
+
+        CalculateDirectionAndDistance();
+        PerformPhysicalMovement();
+
+        GroundPlayer();
+
+        RotatePlayerWithCamera();
+    }
+
+    public void ProcessInput()
+    {
+        playerMovement.leftAndRight = Input.GetAxis("Horizontal"); // A and D
+        playerMovement.forwardAndBackward = Input.GetAxis("Vertical"); // W and S
+
+        playerMovement.movingForwards = playerMovement.forwardAndBackward > 0.0f;
+        playerMovement.movingBackwards = playerMovement.forwardAndBackward < 0.0f;
+
+        bool running = (playerMovement.movingForwards && Input.GetKey(KeyCode.LeftShift))
+        || (!playerMovement.movingBackwards && (playerMovement.leftAndRight > 0.0f || playerMovement.leftAndRight < 0.0f));
+
+        if (running)
+        {
+            playerMovement.speed = playerMovement.baseSpeed * playerMovement.runningAmplifier;
+        }
+        else{
+            playerMovement.speed = playerMovement.baseSpeed;
+
+            playerMovement.forwardAndBackward = playerMovement.forwardAndBackward / 2.0f;
+        }
+    }
+
+    public void PerformBlendTreeAnimation()
+    {
+        float leftAndRight = playerMovement.leftAndRight;
+
+        if (playerMovement.movingBackwards)
+        {
+            leftAndRight = 0.0f;
+        }
+
+        animator.SetFloat("leftAndRight", leftAndRight);
+        animator.SetFloat("forwardAndBackward", playerMovement.forwardAndBackward);
+    }
+
+    public void CalculateDirectionAndDistance()
+    {
+        Vector3 moveDirectionForward = transform.forward * playerMovement.forwardAndBackward;
+        Vector3 moveDirectionSide = transform.right * playerMovement.leftAndRight;
+
+        playerMovement.direction = moveDirectionForward + moveDirectionSide;
+        playerMovement.normalizedDirection = playerMovement.direction.normalized;
+
+        playerMovement.distance = playerMovement.normalizedDirection * playerMovement.speed * Time.deltaTime;
+    }
+
+    public void PerformPhysicalMovement()
+    {
+        characterController.Move(playerMovement.distance);
+    }
+
+    public void GroundPlayer()
+    {
+        if (characterController.isGrounded)
+        {
+            airTime = 0;
+        }
+        else
+        {
+            airTime += Time.deltaTime;
+            Vector3 direction = playerMovement.normalizedDirection;
+
+            direction.y += 0.5f * gravity * airTime;
+
+            playerMovement.normalizedDirection = direction;
+            playerMovement.distance = playerMovement.normalizedDirection * airTime;
+        }
+    }
+
+    public void RotatePlayerWithCamera()
+    {
+        Vector3 rotation;
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            return;
+        }
+        else
+        {
+            rotation = Camera.main.transform.eulerAngles;
+            rotation.x = 0;
+            rotation.z = 0;
+
+            transform.eulerAngles = rotation;
+        }
+    }
+}
