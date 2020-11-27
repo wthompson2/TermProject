c501fab3091f4f40c235e407dd0614b66aa433f7
Rosemary Keenan <rosemary.lillian.keenan@gmail.com>
2020-11-26 22:03:34 -0600

    SMALL VEGAS UPDATE

---
+++ BIG VEGAS ADVENTURES/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static PlayerManager Instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    #endregion
+
+    public GameObject player;
+}
---
+++ BIG VEGAS ADVENTURES/Assets/Scripts/SmallerVegas.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class SmallerVegas : MonoBehaviour
+{
+
+    public float lookRadius = 8f;
+
+    Transform target;
+    NavMeshAgent agent;
+    AudioSource groan;
+    Animator animator;
+
+
+    void Start()
+    {
+        target = PlayerManager.Instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+        groan = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+            //PlaySound();
+            animator.SetFloat("forward", 1.0f);
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
+            //groan.Stop();
+            animator.SetFloat("forward", 0.0f);
+        }
+
+        if (distance <= agent.stoppingDistance)
+        {
+            animator.SetFloat("forward", 0.0f);
+            FaceTarget();
+        }
+    }
+
+    /* void PlaySound()
+    {
+        if (!groan.isPlaying)
+        {
+            groan.Play();
+        }
+    }
+    */
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
+
